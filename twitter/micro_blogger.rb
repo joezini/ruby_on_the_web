724e374d0da0b41c463c05cd15c25b00e1d954a8
bigthinjoe <josephlukeedwards@gmail.com>
2016-05-09 22:44:28 +0100

    Implement jumpstarter microblogger tutorial up to the end of iteration 2

---
+++ twitter/micro_blogger.rb
@@ -0,0 +1,38 @@
+require 'jumpstart_auth'
+
+class MicroBlogger
+	attr_reader :client
+
+	def initialize
+		puts "Initializing..."
+		@client = JumpstartAuth.twitter
+	end
+
+	def tweet(message)
+		if message.length <= 140
+			@client.update(message)
+		else 
+			puts "Tweet '#{message}' is too long!"
+		end
+	end
+
+	def run
+		puts "Welcome to the JSL Twitter Client!"
+		command = ""
+		while command != "q"
+			printf "enter command: "
+			input = gets.chomp
+			parts = input.split(" ")
+			command = parts[0]
+			case command
+			when "q" then puts "Goodbye!"
+			when "t" then tweet(parts[1..-1].join(" "))
+			else
+				puts "Sorry, I don't know how to #{command}"
+			end
+		end
+	end
+end
+
+blogger = MicroBlogger.new
+blogger.run
